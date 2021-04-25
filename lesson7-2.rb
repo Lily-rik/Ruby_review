94ce3ac3b3bfabbaf835da2cebd95d665b34711e
EC2 Default User <violet794.py16@gmail.com>
2021-04-25 01:56:28 +0000

    [anser]2-7

--- lesson7-2.rb
+++ lesson7-2.rb
@@ -3,8 +3,8 @@ puts "何回繰り返しますか？"
 number = gets.to_i
 
 i = 1
-while i <= number do
-  if i == number
+while i <= number do #if文がなくてもこの文でi=numberまでの間が定義されている
+  if i == number #この文が重複している。ここからendまで不要。
     puts "#{i}回目の計算"
     puts "2つの値を入力してください"
 
@@ -22,8 +22,8 @@ while i <= number do
     puts "a/b=#{a / b}"
 
     puts "処理を終了します"
-    break
-  end
+    break 
+  end #ここまで不要
 
   puts "#{i}回目の計算"
   puts "2つの値を入力してください"
@@ -43,3 +43,33 @@ while i <= number do
 
   i += 1
 end
+
+
+#anser
+
+puts "計算をはじめます"
+puts "何回繰り返しますか？"
+
+input = gets.to_i
+
+i = 1
+while i <= input do # i=inputになれば自動でendになるためif文不要
+  puts "#{i}回目の計算"
+  puts "2つの値を入力してください"
+
+  a = gets.to_i
+  b = gets.to_i
+
+  puts "a=#{a}"
+  puts "b=#{b}"
+
+  puts "計算結果を出力します"
+  puts "a+b=#{a + b}"
+  puts "a-b=#{a - b}"
+  puts "a*b=#{a * b}"
+  puts "a/b=#{a / b}"
+
+  i += 1
+end
+
+puts "計算を終了します" #end後に行いたい処理はend後にそのまま書けば
